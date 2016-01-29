988b2d2d28f0f55308162b8e1e6d0cbead83967c
刀哥 <liufan321@gmail.com>
2016-01-29 15:38:13 +0800

    添加图像查看控制器，设置视图层次结构

--- HMImagePicker/HMImagePicker/HMPreviewViewController.m
+++ HMImagePicker/HMImagePicker/HMPreviewViewController.m
@@ -7,17 +7,47 @@
 //
 
 #import "HMPreviewViewController.h"
+#import "HMViewerViewController.h"
 
 @interface HMPreviewViewController ()
 
 @end
 
-@implementation HMPreviewViewController
+@implementation HMPreviewViewController {
+    UIPageViewController *_pageController;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
+    [self prepareChildControllers];
+}
+
+#pragma mark - 准备子控制器
+- (void)prepareChildControllers {
+    
+    // 实例化分页控制器 - 水平分页滚动
+    _pageController = [[UIPageViewController alloc]
+                       initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
+                       navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
+                       options:nil];
+    
+    HMViewerViewController *viewer = [[HMViewerViewController alloc] init];
+    NSArray *viewControllers = @[viewer];
+    
+    // 添加分页控制器的子视图控制器数组
+    [_pageController setViewControllers:viewControllers
+                              direction:UIPageViewControllerNavigationDirectionForward
+                               animated:NO
+                             completion:nil];
+    
+    [self addChildViewController:_pageController];
+    [self.view addSubview:_pageController.view];
+    
+    [_pageController didMoveToParentViewController:self];
+    
+    self.view.gestureRecognizers = _pageController.gestureRecognizers;
 }
 
 @end
--- HMImagePicker/HMImagePicker/HMViewerViewController.m
+++ HMImagePicker/HMImagePicker/HMViewerViewController.m
@@ -12,10 +12,29 @@
 
 @end
 
-@implementation HMViewerViewController
+@implementation HMViewerViewController {
+    UIScrollView *_scrollView;
+    UIImageView *_imageView;
+}
+
+- (void)loadView {
+    _scrollView = [[UIScrollView alloc] init];
+    self.view = _scrollView;
+    
+    _imageView = [[UIImageView alloc] init];
+    [_scrollView addSubview:_imageView];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _imageView.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    _imageView.frame = _scrollView.bounds;
 }
 
 @end
